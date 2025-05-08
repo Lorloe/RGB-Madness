ecdf3a2f4c1ea5eeb7d7db13607fe8e1cabef244
Thi Kha <89496513+Lorloe@users.noreply.github.com>
2025-05-08 16:03:28 +0700

    Update Singleton.cs

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     // Generic singleton
-    public static T Instance { get ; private set;}
+    public static T Instance { get ; private set; }
 
     protected virtual void Awake() 
     {
